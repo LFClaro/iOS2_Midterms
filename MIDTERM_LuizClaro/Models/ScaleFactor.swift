913f0fda68eadc98abfb1ab03d71d3d06ff53360
Luiz Claro <lfclaro@gmail.com>
2022-03-10 04:35:01 -0500

    commiting

---
+++ MIDTERM_LuizClaro/Models/ScaleFactor.swift
@@ -0,0 +1,13 @@
+//
+//  ScaleFactor.swift
+//  MIDTERM_LuizClaro
+//
+//  Created by Luiz Fernando Reis on 2022-03-08.
+//
+
+import SwiftUI
+
+struct ScaleFactor {
+    var w = UIScreen.main.bounds.size.width
+    var h = UIScreen.main.bounds.size.height
+}
